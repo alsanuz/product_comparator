399d03b9447aaac77457050275bab90c8041721d
alsanuz <alsanuz@hotmail.com>
2022-03-30 16:33:55 +0100

    update settings to do a test

--- product_comparator/settings.py
+++ product_comparator/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'product_comparator',
         'USER': 'postgres',
-        'PASSWORD': '',
+        'PASSWORD': 'test',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
